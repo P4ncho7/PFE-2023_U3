6db00998fd900c6de7e6c811031ad70033af6709
FRANCISCO IGNACIO TOLEDO ALVAREZ <francisco.toledo10@inacapmail.cl>
2023-07-10 18:25:38 -0400

    Añadidas rutas y componentes

---
+++ src/components/Calama.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from "react"
+
+const Calama = () => {
+
+    return(
+        <>
+        <h1>Calama</h1>
+        </>
+    )
+
+}
+
+export default Calama
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from "react";
+
+/* view */
+
+const HolaMundo = () => {
+
+    return(
+        <>
+        <h1>Hola Mundo</h1>
+        </>
+    )
+
+}
+
+export default HolaMundo
